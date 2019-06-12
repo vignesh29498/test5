5c0acc98a417c1cfd7f95ddc3e971a043578d454
vignesh29498@gmail.com <vignesh29498@gmail.com>
2019-06-12 17:35:04 +0530

    first commit

---
+++ db/migrate/20190608140240_create_employees.rb
@@ -0,0 +1,13 @@
+class CreateEmployees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employees do |t|
+      t.string :name
+      t.string :email
+      t.integer :age
+      t.string :gender
+      t.string :designation
+
+      t.timestamps
+    end
+  end
+end
